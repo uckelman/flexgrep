c5a0fe724f088ce1ea9b09c6580902f73ddea807
Joel Uckelman <uckelman@nomic.net>
2012-03-15 02:53:17 +0100

    Use std::cout for output. Make pos wide enough for large offsets.

--- buildlex.pl
+++ buildlex.pl
@@ -3,24 +3,32 @@
 # definitions
 print <<EOF;
 %{
-  int pos = 0;
+#include <cstdint>
+#include <iostream>
+
+uint64_t pos = 0;
+
+#define YY_READ_BUF_SIZE 16777216
 %}
 
-%option 8bit main nodefault nounput noyylineno warn
+%option 8bit main nodefault nounput noyylineno noyymore noyywrap warn
 
 %%
+
 EOF
 
 # rules
 while (<ARGV>) {
   chomp;
   print <<"EOF";
-$_  printf("@{[$.-1]}@[%d,%d): %s\\n", pos, pos + yyleng, yytext); pos += yyleng;
+$_  std::cout << "@{[$.-1]}@[" << pos << ',' << (pos + yyleng) << "): " << yytext << '\\n'; pos += yyleng;
+
 EOF
 }
 
 # catchall rule
 print <<EOF;
 .|\\n  ++pos; /* eat unmatched chars */
+
 %%
 EOF
