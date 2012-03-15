cb256d57deed62ad4a3fa8a563c15d7b395525be
Joel Uckelman <uckelman@nomic.net>
2012-03-15 04:04:49 +0100

    Set flex input buffer to 16MB. Use same flex options as Bulk Extractor. Define our own YY_INPUT to indicate how far along we are.

--- buildlex.pl
+++ buildlex.pl
@@ -8,10 +8,32 @@ print <<EOF;
 
 uint64_t pos = 0;
 
+#define YY_SKIP_YYWRAP
+
+#undef YY_BUF_SIZE
+#define YY_BUF_SIZE 16777216
+
 #define YY_READ_BUF_SIZE 16777216
+
+#define YY_INPUT(buf,result,max_size) \\
+	errno=0; \\
+	while ( (result = read( fileno(yyin), (char *) buf, max_size )) < 0 ) \\
+	{ \\
+		if( errno != EINTR) \\
+		{ \\
+			YY_FATAL_ERROR( "input in flex scanner failed" ); \\
+			break; \\
+		} \\
+		errno=0; \\
+		clearerr(yyin); \\
+	}\\
+  std::cerr << pos << std::endl; \\
+\\
+
 %}
 
-%option 8bit main nodefault nounput noyylineno noyymore noyywrap warn
+/* %option 8bit main nodefault nounput noyylineno noyymore noyywrap warn */
+%option 8bit main pointer batch noyymore noyywrap case-insensitive
 
 %%
 
